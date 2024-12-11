1c3f02374bb7f3a027440f2ceca12c175e6ebbf6
harits-husnandar <haritz90517@gmail.com>
2024-12-11 11:45:19 +0700

    update 11 - 12 -2024

--- src/context/MovieContext.tsx
+++ src/context/MovieContext.tsx
@@ -1,3 +1,6 @@
+import { createContext, useContext, useState } from "react"
+import { getMovieDetails, searchMovies } from "../api/omdbApi"
+
 interface Movie  {
         Title: string
         Year: string
@@ -18,8 +21,57 @@ interface MovieContextType {
     movies: Movie[]
     selectedMovie: MovieDetail | null
     searchMovies: (query:string)=>Promise<void>
-    selectedMovies: (id:string)=>Promise<void>
+    selectMovie: (id:string)=>Promise<void>
     closeMovie: ()=>void
 }
 
-const MovieContext = createContext
+const MovieContext = createContext<MovieContextType | undefined>(undefined)
+
+type MovieProviderProps ={
+    children : React.ReactNode
+}
+
+export const MovieProvider = ({children}:MovieProviderProps)=>{
+    // state dari movies
+    const [movies, setMovies] = useState<Movie[]>([])
+
+    // state selectedMovie
+    const [selectedMovie, setSelectedMovie] = useState<MovieDetail | null>(null)
+
+    // search movies
+    const searchMoviesHandler = async (query:string) =>{
+        const result = await searchMovies(query) 
+        setMovies(result || [])
+    }
+
+    const selectedMoviesHandler = async(id:string)=>{
+        const movieDetails = await getMovieDetails(id)
+        setSelectedMovie(movieDetails)
+    }
+
+    const closeMovieHandler = ()=>{
+        setSelectedMovie(null)
+    }
+
+    return(
+        <MovieContext.Provider value={{
+            movies,
+            selectedMovie,
+            searchMovies: searchMoviesHandler,
+            selectMovie: selectedMoviesHandler,
+            closeMovie: closeMovieHandler
+        }}>
+            {children}
+        </MovieContext.Provider>
+    )
+}
+
+// custon hooks untuk useContext
+export const useMovieContext =()=>{
+    const context = useContext(MovieContext)
+    if(context === undefined){
+        throw new Error('useMovieContext must be used within a MovieProvider')
+    }
+    return context
+}
+
